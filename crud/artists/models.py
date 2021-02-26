bef103502b75e5682850e30c316f09e0d9ce5774
Rastrian <me@rastrian.dev>
2021-02-26 17:23:24 -0300

    Fixed issues with null parameters

--- crud/artists/models.py
+++ crud/artists/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 
 class Artist(models.Model):
-    name = models.CharField(max_length=30, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    name = models.CharField(max_length=30, null=False)
+    date_created = models.DateTimeField(auto_now_add=True, null=False)
     def __str__(self):
         return self.name
 
 class Music(models.Model):
-    title = models.CharField(max_length=50, null=True)
+    title = models.CharField(max_length=50, null=False)
     duration = models.DecimalField(decimal_places=2, max_digits= 100000)
-    spotify_published = models.BooleanField(null=True)
-    youtube_published = models.BooleanField(null=True)
-    foreign_key = models.ForeignKey(Artist, null=True, on_delete=models.CASCADE, default=None)
+    spotify_published = models.BooleanField(null=False)
+    youtube_published = models.BooleanField(null=False)
+    foreign_key = models.ForeignKey(Artist, null=False, on_delete=models.CASCADE, default=None)
 
 
     def __str__(self):
